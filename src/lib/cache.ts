35ba33a67059c140cfca736a0b4661a0565e4a21
Genna Weber <62393667+gennaweber@users.noreply.github.com>
2023-05-21 11:04:07 -0400

    Add env variable for SUPABASE_URL

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { APIOutput } from '../types';
 
-const SUPABASE_URL = 'https://iqdsdnumpussaltfrvgb.supabase.co';
+const SUPABASE_URL = process.env.SUPABASE_URL;
 
 const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY!);
 
